b072c59e447834ec937009573abf0af2daf1a494
Andrew Chin <chin@juniper.net>
2015-03-27 21:07:54 -0500

    Packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='osx-cert-utils',
+    version='0.1',
+    description='Tools to manage OS X SystemRootCertificates keychain.',
+    author='Andrew Chin',
+    author_email='andrewtchin@gmail.com',
+    license='MIT',
+    packages=['osx_cert_utils'],
+)
